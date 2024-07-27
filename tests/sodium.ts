61c3f33c9a0f4fb23c2a84563d9d8d93021ed5f5
Audric Ackermann <audric@getsession.org>
2024-07-27 12:30:30 +1000

    feat: fix working v1 with session-tooling. Still need to use it

---
+++ tests/sodium.ts
@@ -0,0 +1,8 @@
+import * as libSodiumWrappers from 'libsodium-wrappers-sumo';
+
+export type LibSodiumType = typeof libSodiumWrappers;
+
+export async function getSodiumNode() {
+  await libSodiumWrappers.ready;
+  return (libSodiumWrappers as any).default as LibSodiumType;
+}
