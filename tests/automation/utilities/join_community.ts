1b6e405a5dd8d79babfeac316ea6e2a3a23c6948
wafflesvsfrankie <92288602+burtonemily@users.noreply.github.com>
2024-12-18 16:54:02 +1100

    Replaces test tag and fixes reporter to show stacktrace on failure

--- tests/automation/utilities/join_community.ts
+++ tests/automation/utilities/join_community.ts
@@ -6,6 +6,6 @@ export const joinCommunity = async (window: Page) => {
   await clickOnTestIdWithText(window, 'new-conversation-button');
   await clickOnTestIdWithText(window, 'chooser-new-community');
   //   The follow two test tags are pending implementation
-  await typeIntoInput(window, 'new-community-conversation', testCommunityLink);
+  await typeIntoInput(window, 'join-community-conversation', testCommunityLink);
   await clickOnTestIdWithText(window, 'join-community-button');
 };
