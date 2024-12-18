d51178f07f8e99bf513fa8bb76ad7c23071e1684
wafflesvsfrankie <92288602+burtonemily@users.noreply.github.com>
2024-12-18 15:04:00 +1100

    Adds in test to join community and to send an image

--- tests/automation/types/testing.ts
+++ tests/automation/types/testing.ts
@@ -166,4 +166,8 @@ export type DataTestId =
   | DMTimeOption
   | `input-${DMTimeOption}`
   | 'disappear-messages-type-and-time'
-  | 'hide-recovery-password-button';
+  | 'hide-recovery-password-button'
+  | 'chooser-new-community'
+  | 'new-community-conversation'
+  | 'join-community-button'
+  | 'scroll-to-bottom-button';
