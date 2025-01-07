11d4d5836ed2f68edd4a1b13df5eca83e234ee45
wafflesvsfrankie <92288602+burtonemily@users.noreply.github.com>
2025-01-07 14:31:29 +1100

    Fixing up merge conflicts

--- tests/automation/message_checks.spec.ts
+++ tests/automation/message_checks.spec.ts
@@ -249,68 +249,11 @@ sessionTestTwoWindows(
 test_Alice_1W_Bob_1W(
   'Send link 1:1',
   async ({ alice, aliceWindow1, bob, bobWindow1 }) => {
-    const testMessage = 'https://getsession.org/';
+    const testLink = 'https://getsession.org/';
     const testReply = `${bob.userName} replying to link from ${alice.userName}`;
 
     await createContact(aliceWindow1, bobWindow1, alice, bob);
-    await typeIntoInput(aliceWindow1, 'message-input-text-area', testMessage);
-    await clickOnElement({
-      window: aliceWindow1,
-      strategy: 'data-testid',
-      selector: 'send-message-button',
-    });
-    await clickOnTestIdWithText(
-      aliceWindow1,
-      'message-content',
-      testMessage,
-      true,
-    );
-    // Need to copy link to clipboard, as the enable link preview modal
-    // doesn't pop up if manually typing link (needs to be pasted)
-    // Need to have a nth(0) here to account for Copy Account ID, Appium was getting confused
-    const firstCopyBtn = await aliceWindow1
-      .locator(
-        `[data-testid=context-menu-item]:has-text(${englishStrippedStr(
-          'copy',
-        ).toString()})`,
-      )
-      .nth(0);
-    await firstCopyBtn.click();
-    await waitForTestIdWithText(
-      aliceWindow1,
-      'session-toast',
-      englishStrippedStr('copied').toString(),
-    );
-    await clickOnTestIdWithText(aliceWindow1, 'message-input-text-area');
-    const isMac = process.platform === 'darwin';
-    await aliceWindow1.keyboard.press(`${isMac ? 'Meta' : 'Control'}+V`);
-    // No test tag on modal-description
-    // await checkModalStrings(
-    //   aliceWindow1,
-    //   englishStrippedStr('linkPreviewsEnable').toString(),
-    //   englishStrippedStr('linkPreviewsFirstDescription')
-    //     .withArgs({ app_name: 'Session' })
-    //     .toString(),
-    // );
-    await clickOnTestIdWithText(
-      aliceWindow1,
-      'session-confirm-ok-button',
-      englishStrippedStr('enable').toString(),
-    );
-    // Needs to be changed to link-preview-loading
-    await waitForLoadingAnimationToFinish(aliceWindow1, 'loading-spinner');
-    // Also needs to be implemented
-    // await waitForTestIdWithText(aliceWindow1, 'link-preview-image')
-    // await waitForTestIdWithText(
-    //   aliceWindow1,
-    //   'link-preview-title',
-    //   'Session | Send Messages, Not Metadata. | Private Messenger',
-    // );
-    await clickOnElement({
-      window: aliceWindow1,
-      strategy: 'data-testid',
-      selector: 'send-message-button',
-    });
+    await sendLinkPreview(aliceWindow1, testLink);
     await waitForElement(
       bobWindow1,
       'class',
@@ -320,7 +263,7 @@ test_Alice_1W_Bob_1W(
     );
     await replyTo({
       senderWindow: bobWindow1,
-      textMessage: testMessage,
+      textMessage: testLink,
       replyText: testReply,
       receiverWindow: aliceWindow1,
     });
