52b89ca7380df4099b02aa84b7571ef33a4db0d4
Audric Ackermann <audric@getsession.org>
2025-09-25 07:13:00 +0200

    chore: remove uneeded sharp dep

--- tests/automation/landing_page.spec.ts
+++ tests/automation/landing_page.spec.ts
@@ -5,12 +5,6 @@ import {
   waitForElement,
 } from './utilities/utils';
 
-// TODO: Normalize screenshot dimensions before comparison to handle different pixel densities (e.g. with sharp)
-// This would fix MacBook Retina (2x) vs M4 Mac Mini (1x) pixel density differences (1000x1584 vs 500x792)
-// Alternatives:
-// - Try to set deviceScaleFactor: 1 in Playwright context to force consistent scaling
-// - Record pixel density dependent screenshots
-
 test_Alice_2W(
   `Landing page states`,
   async ({ aliceWindow1, aliceWindow2 }, _testInfo) => {
