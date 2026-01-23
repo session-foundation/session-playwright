b718ee6119bdd7c883ce730542021d11333908f9
Miklos Mandoki <miklos@getsession.org>
2026-01-23 14:45:44 +1100

    chore: add linked device test

--- tests/automation/recovery_phrase_banner.spec.ts
+++ tests/automation/recovery_phrase_banner.spec.ts
@@ -1,11 +1,13 @@
 import type { Page } from '@playwright/test';
 
+import { sleepFor } from '../promise_utils';
 import { Global, HomeScreen, Settings } from './locators';
 import { test_Alice_1W } from './setup/sessionTest';
 import {
   joinDefaultCommunity,
   leaveCommunity,
 } from './utilities/join_community';
+import { linkedDevice } from './utilities/linked_device';
 import {
   clickOn,
   hasElementPoppedUpThatShouldnt,
@@ -28,7 +30,7 @@ async function bannerShouldAppear(window: Page) {
     window,
     HomeScreen.revealRecoveryPhraseButton.selector,
   );
-  console.log('Banner is visible');
+  console.log('On home screen, banner is visible');
 }
 
 test_Alice_1W(
@@ -41,6 +43,18 @@ test_Alice_1W(
     await bannerShouldNotAppear(aliceWindow1);
     await joinDefaultCommunity(aliceWindow1, 'Session Updates');
     await bannerShouldAppear(aliceWindow1);
+  },
+);
+
+test_Alice_1W(
+  'Recovery password banner 2 windows',
+  async ({ aliceWindow1, alice }) => {
+    await joinDefaultCommunity(aliceWindow1, 'Lokinet Updates');
+    await joinDefaultCommunity(aliceWindow1, 'Session Network Updates');
+    await joinDefaultCommunity(aliceWindow1, 'Session Updates');
+    const aliceWindow2 = await linkedDevice(alice.recoveryPassword);
+    await sleepFor(2_000);
+    await bannerShouldNotAppear(aliceWindow2);
   },
 );
 
