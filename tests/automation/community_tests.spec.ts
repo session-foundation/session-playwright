58720c29ef31649fb3e640c4b16784fea92a9abf
Miklos Mandoki <miklos@getsession.org>
2026-02-05 15:01:29 +1100

    feat: add ban/unban tests

--- tests/automation/community_tests.spec.ts
+++ tests/automation/community_tests.spec.ts
@@ -1,11 +1,28 @@
 import { testCommunityName } from './constants/community';
-import { Conversation, HomeScreen } from './locators';
-import { test_Alice_1W_Bob_1W, test_Alice_2W } from './setup/sessionTest';
-import { joinCommunity } from './utilities/join_community';
-import { sendMessage } from './utilities/message';
+import { Conversation, Global, HomeScreen } from './locators';
+import { newUser } from './setup/new_user';
+import { recoverFromSeed } from './setup/recovery_using_seed';
+import {
+  sessionTestTwoWindows,
+  test_Alice_1W_Bob_1W,
+  test_Alice_2W,
+} from './setup/sessionTest';
+import {
+  assertAdminIsKnown,
+  joinCommunity,
+  joinOrOpenCommunity,
+} from './utilities/join_community';
+import { sendMessage, waitForFailedTick } from './utilities/message';
 import { replyTo } from './utilities/reply_message';
 import { sendMedia } from './utilities/send_media';
-import { clickOn, clickOnWithText } from './utilities/utils';
+import {
+  clickOn,
+  clickOnWithText,
+  hasElementBeenDeleted,
+  hasElementPoppedUpThatShouldnt,
+  typeIntoInput,
+  waitForTestIdWithText,
+} from './utilities/utils';
 
 test_Alice_2W(
   'Join community and sync',
@@ -48,3 +65,105 @@ test_Alice_1W_Bob_1W(
     });
   },
 );
+
+sessionTestTwoWindows('Ban User', async ([windowA, windowB]) => {
+  assertAdminIsKnown();
+  const msg1 = `Ban me! - ${Date.now()}`;
+  const msg2 = `Am I still here? - ${Date.now()}`;
+  await Promise.all([
+    recoverFromSeed(windowA, process.env.SOGS_ADMIN_SEED!, {
+      fallbackName: 'Admin',
+    }),
+    newUser(windowB, 'Bob'),
+  ]);
+  await Promise.all([joinOrOpenCommunity(windowA), joinCommunity(windowB)]);
+  await sendMessage(windowB, msg1);
+  await clickOnWithText(windowA, Conversation.messageContent, msg1, {
+    rightButton: true,
+  });
+  await windowA.bringToFront()
+  await clickOnWithText(windowA, Global.contextMenuItem, 'Ban User', {
+    strictMode: false,
+  });
+  await clickOn(windowA, Conversation.banUserButton);
+  await typeIntoInput(windowB, Conversation.messageInput.selector, msg2);
+  await clickOn(windowB, Conversation.sendMessageButton);
+  await waitForFailedTick(windowB, msg2);
+  await hasElementPoppedUpThatShouldnt(
+    windowA,
+    Conversation.messageContent.strategy,
+    Conversation.messageContent.selector,
+    msg2,
+  );
+});
+
+sessionTestTwoWindows('Unban User', async ([windowA, windowB]) => {
+  assertAdminIsKnown();
+  const msg1 = `Ban me but unban me later! - ${Date.now()}`;
+  const msg2 = `I'm banned :( - ${Date.now()}`;
+  const msg3 = `Freedom! - ${Date.now()}`
+  await Promise.all([
+    recoverFromSeed(windowA, process.env.SOGS_ADMIN_SEED!, {
+      fallbackName: 'Admin',
+    }),
+    newUser(windowB, 'Bob'),
+  ]);
+  await Promise.all([joinOrOpenCommunity(windowA), joinCommunity(windowB)]);
+  await sendMessage(windowB, msg1);
+  await clickOnWithText(windowA, Conversation.messageContent, msg1, {
+    rightButton: true,
+  });
+  await clickOnWithText(windowA, Global.contextMenuItem, 'Ban User', {
+    strictMode: false,
+  });
+  await clickOn(windowA, Conversation.banUserButton);
+  await typeIntoInput(windowB, Conversation.messageInput.selector, msg2);
+  await clickOn(windowB, Conversation.sendMessageButton);
+  await waitForFailedTick(windowB, msg2);
+  await clickOnWithText(windowA, Conversation.messageContent, msg1, {
+    rightButton: true,
+  });
+  await clickOnWithText(windowA, Global.contextMenuItem, 'Unban User', {
+    strictMode: false,
+  });
+  await clickOn(windowA, Conversation.unbanUserButton);
+  await sendMessage(windowB, msg3);
+  await waitForTestIdWithText(windowA, Conversation.messageContent.selector, msg3)
+});
+
+sessionTestTwoWindows('Ban And Delete All', async ([windowA, windowB]) => {
+  assertAdminIsKnown();
+  const msg1 = `Ban and delete! - ${Date.now()}`;
+  const msg2 = `Did that work? - ${Date.now()}`;
+  await Promise.all([
+    recoverFromSeed(windowA, process.env.SOGS_ADMIN_SEED!, {
+      fallbackName: 'Admin',
+    }),
+    newUser(windowB, 'Bob'),
+  ]);
+  await Promise.all([joinOrOpenCommunity(windowA), joinCommunity(windowB)]);
+  await sendMessage(windowB, msg1);
+  await clickOnWithText(windowA, Conversation.messageContent, msg1, {
+    rightButton: true,
+  });
+  await clickOnWithText(windowA, Global.contextMenuItem, 'Ban User', {
+    strictMode: false,
+  });
+  await clickOn(windowA, Conversation.banAndDeleteAllButton);
+  await hasElementBeenDeleted(
+    windowA,
+    Conversation.messageContent.strategy,
+    Conversation.messageContent.selector,
+    10_000,
+    msg1,
+  );
+  await typeIntoInput(windowB, Conversation.messageInput.selector, msg2);
+  await clickOn(windowB, Conversation.sendMessageButton);
+  await waitForFailedTick(windowB, msg2);
+  await hasElementPoppedUpThatShouldnt(
+    windowA,
+    Conversation.messageContent.strategy,
+    Conversation.messageContent.selector,
+    msg2,
+  );
+});
--- tests/automation/locators/index.ts
+++ tests/automation/locators/index.ts
@@ -82,6 +82,11 @@ export class Conversation extends Locator {
   static readonly acceptMessageRequestButton = this.testId(
     'accept-message-request',
   );
+  static readonly banAndDeleteAllButton = this.testId(
+    'ban-user-delete-all-confirm-button',
+  );
+  static readonly banUserButton = this.testId('ban-user-confirm-button');
+  static readonly banUserInput = this.testId('ban-user-input');
   static readonly blockMessageRequestButton = this.testId(
     'decline-and-block-message-request',
   );
@@ -101,14 +106,15 @@ export class Conversation extends Locator {
   static readonly mentionsContainer = this.testId('mentions-container'); // This is also the locator for emojis
   static readonly mentionsItem = this.testId('mentions-container-row'); // This is also the locator for emojis
   static readonly messageContent = this.testId('message-content');
-
   static readonly messageInput = this.testId('message-input-text-area');
+
   static readonly messageRequestAcceptControlMessage = this.testId(
     'message-request-response-message',
   );
   static readonly microphoneButton = this.testId('microphone-button');
   static readonly scrollToBottomButton = this.testId('scroll-to-bottom-button');
   static readonly sendMessageButton = this.testId('send-message-button');
+  static readonly unbanUserButton = this.testId('unban-user-confirm-button')
 }
 
 export class ConversationSettings extends Locator {
